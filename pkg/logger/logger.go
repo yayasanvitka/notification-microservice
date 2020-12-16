fb5389281c9745fa6d4db977b0e42738d81e826c
shadowbane <adly.shadowbane@gmail.com>
2020-12-16 11:17:01 +0700

    Create log by date run

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -5,31 +5,54 @@ import (
 	"go.uber.org/zap/zapcore"
 	"log"
 	"os"
+	"time"
 )
 
 var (
-	Info     = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-	Error    = log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	Info  = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	Error = log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 )
 
-func initZapLog() *zap.Logger {
-	config := zap.NewDevelopmentConfig()
-	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
-    config.EncoderConfig.TimeKey = "timestamp"
-    config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-    config.OutputPaths = []string{
-    	"log/app.log",
-    	"stdout",
-	}
-	config.ErrorOutputPaths = []string{
-    	"log/app.log",
-    	"stderr",
-	}
-    zapLogger, _ := config.Build()
-    return zapLogger
+type LogFile struct {
+	file string
 }
 
-func Init()  {
+func createLogFile(l LogFile) {
+	f, err := os.OpenFile(l.file, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f.Close()
+
+}
+
+func initZapLog() *zap.Logger {
+	t := time.Now()
+	formattedTime := t.Format("2006-01-02")
+	logfile := LogFile{
+		"log/" + formattedTime + ".log",
+	}
+
+	createLogFile(logfile)
+
+	config := zap.NewDevelopmentConfig()
+	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	config.EncoderConfig.TimeKey = "timestamp"
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.OutputPaths = []string{
+		logfile.file,
+		"stdout",
+	}
+	config.ErrorOutputPaths = []string{
+		logfile.file,
+		"stderr",
+	}
+	zapLogger, _ := config.Build()
+	return zapLogger
+}
+
+func Init() {
 	logManager := initZapLog()
 	zap.ReplaceGlobals(logManager)
 	defer logManager.Sync() // flushes buffer, if any
